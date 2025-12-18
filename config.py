6b912125811772e7d295e024a31b4ae3761e18f9
Rahul-Khan-619 <igfollowers070+khan@gmail.com>
2025-12-18 16:37:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,9 +16,10 @@ SESSION_NAME = "userbot_session"
 # Configuration
 # The Group IDs to monitor. 
 # Add IDs to the list. Set to empty list [] to monitor ALL groups (and see their IDs printed).
-SOURCE_GROUP_ID = [3549831857,3344273206]
+SOURCE_GROUP_IDS = [3549831857,3344273206]
 
 # Optional: Pre-fill phone number to avoid typing it every time (Format: +1234567890)
 # If empty, you will be prompted in the terminal.
 PHONE_NUMBER = "" 
+
 
