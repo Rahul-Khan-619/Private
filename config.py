d495e575ca64f0bb526d2797a4c0fb4703f6c030
Rahul-Khan-619 <igfollowers070+khan@gmail.com>
2026-01-07 18:17:20 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ API_ID = 31349440
 API_HASH = "93811745aee1c809da395285b24ddc73"
 
 # Target Bots
-TARGET_BOT_USERNAME = "@FreeUndreBot" # For main.py (Primary)
-BACKUP_BOT_USERNAME = "@AdvancedDreamCloBot" # For main.py (Backup)
+TARGET_BOT_USERNAME = "@NudeMe8Bot" # For main.py (Primary)
+BACKUP_BOT_USERNAME = "@NudeMe22Bot" # For main.py (Backup)
 VIDEO_BOT_USERNAME = "@Mr_Super_Editor_1roBot" # For video_automation.py
 UNDRESSHER_BOT_USERNAME = "@UndressHer20Bot" # For undressher_automation.py
 
 # The bot to generate links (File Store Bot)
-FILE_STORE_BOT_USERNAME = "@Mr_Super_Edits_robot"
+FILE_STORE_BOT_USERNAME = "@Mr_Super_Human_SafeBot"
 
 # API Bot Settings (@Super_edit_roBot)
 API_BOT_TOKEN = "8576355906:AAFZEeX2-TNvanDE9IJ-SvIghkCm1lqM38o"
@@ -25,18 +25,17 @@ SESSION_NAME = "userbot_session"
 # Set to empty list [] to monitor ALL groups.
 
 # Groups for main.py (@FreeUndreBot & @AdvancedDreamCloBot)
-MAIN_BOT_GROUPS = [3344273206, 3549831857]
+MAIN_BOT_GROUPS = [3615852792]
 
 # Groups for video_automation.py (@Mr_Super_Editor_1roBot)
-VIDEO_BOT_GROUPS = [3344273206]
+VIDEO_BOT_GROUPS = [3615852792]
 
 # Groups for undressher_automation.py (@UndressHer20Bot)
-UNDRESSHER_BOT_GROUPS = [3344273206]
+UNDRESSHER_BOT_GROUPS = [3615852792]
 
 # Groups for api_bot.py (@Super_edit_roBot)
-API_BOT_GROUPS = [3344273206, 3549831857]
+API_BOT_GROUPS = [3615852792]
 
 # Optional: Pre-fill phone number to avoid typing it every time (Format: +1234567890)
 # If empty, you will be prompted in the terminal.
 PHONE_NUMBER = "" 
-
--- main.py
+++ main.py
@@ -179,7 +179,8 @@ async def process_photo_with_bot(bot_username, source_chat_id, message):
                         print(f"[{bot_username}] Link: {final_url}")
                         
                         promo_link = random.choice(PROMO_LINKS) if PROMO_LINKS else "https://t.me/Mr_Super_Edits_robot"
-                        caption = f"Your Edited Photo Result is here 👉 {final_url}\n👇👇👇👇\n{promo_link} \n 👆👆 Do You want to Do a Video Edit Like This ?\nMsg to @Mr_Super_Editor"
+                        # caption = f"Your Edited Photo Result is here 👉 {final_url}\n👇👇👇👇\n{promo_link} \n 👆👆 Do You want to Do a Video Edit Like This ?\nMsg to @Mr_Super_Editor"
+                        caption = f"Your Edited Photo Result is here 👉 {final_url} \n Dm to @Mr_Super_Human And Get Photo to Video Edit Bot at less Price \n msg to @Mr_Super_Human Now "
                         
                         await client.send_message(source_chat_id, caption, reply_to=message.id)
                         print(f"[{bot_username}] Done!")
