4950dc2d56540b775676b69392e0e8e56a42b3b3
Rahul-Khan-619 <igfollowers070+khan@gmail.com>
2026-07-12 17:51:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = 31349440
 API_HASH = "93811745aee1c809da395285b24ddc73"
 
 # Target Bots
-TARGET_BOT_USERNAME = "@IRNoodSaz_02Bot" # For main.py (Primary)
+TARGET_BOT_USERNAME = "@EasyAI63_Bot" # For main.py (Primary)
 BACKUP_BOT_USERNAME = "@EasyAI62_Bot" # For main.py (Backup)
 VIDEO_BOT_USERNAME = "@Mr_Super_Editor_1roBot" # For video_automation.py
 UNDRESSHER_BOT_USERNAME = "@UndressHer20Bot" # For undressher_automation.py
