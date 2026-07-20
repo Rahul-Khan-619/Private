b788452f5702ca6634d9b19d461933285a7f8700
Rahul-Khan-619 <igfollowers070+khan@gmail.com>
2026-07-20 22:01:30 +0530

    Update bot usernames in config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ API_ID = 31349440
 API_HASH = "93811745aee1c809da395285b24ddc73"
 
 # Target Bots
-TARGET_BOT_USERNAME = "@bj690_bot" # For main.py (Primary)
-BACKUP_BOT_USERNAME = "@mynudeus048_bot" # For main.py (Backup)
+TARGET_BOT_USERNAME = "@otherr_acount_bot" # For main.py (Primary)
+BACKUP_BOT_USERNAME = "@Burt6357bot" # For main.py (Backup)
 VIDEO_BOT_USERNAME = "@Mr_Super_Editor_1roBot" # For video_automation.py
 UNDRESSHER_BOT_USERNAME = "@UndressHer20Bot" # For undressher_automation.py
 
